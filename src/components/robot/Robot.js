6ab7c66a5fdf271846a187827b5e9678169a0d17
Vanesssa Mastrosimone <vanesssamastrosimone@Vanesssas-MacBook-Air.local>
2019-02-26 14:28:30 -0800

    added a robot page

---
+++ src/components/robot/Robot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Robot extends Component {
+  render() {
+    return (
+      <div className="Robot">
+        Robot
+      </div>
+    );
+  }
+}
+
+export default Robot;
--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from '../home/Home.js';
 import Calendar from '../calendar/Calendar.js';
 import Login from '../login/Login.js';
+import Robot from '../robot/Robot';
 
 class Content extends Component {
   render() {
@@ -13,6 +14,7 @@ class Content extends Component {
                     <Route exact path="/" component={Home} />
                     <Route path="/login" component={Login} />
                     <Route path="/calendar" component={Calendar} />
+                    <Route path="/robot" component={Robot} />
                 </div>
             </div>
       </div>
