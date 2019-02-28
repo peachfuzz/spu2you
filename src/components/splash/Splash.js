7c1816e98183eb158de0049f6c0700e8bad5f893
Alex Lopez <alxjlopez96@gmail.com>
2019-02-28 13:46:37 -0800

    okay added the splash frfr

---
+++ src/components/splash/Splash.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Splash extends Component {
+  render() {
+    return (
+      <div className="Splash">
+        <p>Splash!!</p>
+      </div>
+    );
+  }
+}
+
+export default Splash;
