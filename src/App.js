b932041835a77d8568ac5da05998bf85122559d7
Hector <hectordm65@gmail.com>
2019-03-02 17:56:33 -0800

    Added prettier to the code and formatted all code to look the same

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import Content from './components/router/Router.js';
-import Header from './components/static/Header.js';
-import Footer from './components/static/Footer.js';
-import SideMenu from './components/static/SideMenu.js';
+import React, { Component } from "react";
+import "./App.css";
+import Content from "./components/router/Router.js";
+import Header from "./components/static/Header.js";
+import Footer from "./components/static/Footer.js";
+import SideMenu from "./components/static/SideMenu.js";
 
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 //import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 //import "@blueprintjs/select/lib/css/blueprint-select.css";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App bp3-dark">
-          <Header/>
-          <SideMenu/>
-          <Content/>
-          <Footer/>
+          <Header />
+          <SideMenu />
+          <Content />
+          <Footer />
         </div>
       </Router>
     );
--- src/components/static/SideMenu.js
+++ src/components/static/SideMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Menu, MenuDivider, MenuItem } from "@blueprintjs/core";
 import { BrowserRouter as Router } from "react-router-dom";
 //docs: https://blueprintjs.com/docs/#blueprint
@@ -8,14 +8,22 @@ class SideMenu extends Component {
     return (
       <div className="App-menu">
         <Menu>
-            <MenuItem icon="home" onClick={this.handleClick} text="Home" />
-            <MenuItem icon="new-object" onClick={this.handleClick} text="New object" />
-            <MenuItem icon="new-link" onClick={this.handleClick} text="New link" />
-            <MenuDivider />
-            <MenuItem text="Settings..." icon="cog">
-                <MenuItem icon="tick" text="Save on edit" />
-                <MenuItem icon="blank" text="Compile on edit" />
-            </MenuItem>
+          <MenuItem icon="home" onClick={this.handleClick} text="Home" />
+          <MenuItem
+            icon="new-object"
+            onClick={this.handleClick}
+            text="New object"
+          />
+          <MenuItem
+            icon="new-link"
+            onClick={this.handleClick}
+            text="New link"
+          />
+          <MenuDivider />
+          <MenuItem text="Settings..." icon="cog">
+            <MenuItem icon="tick" text="Save on edit" />
+            <MenuItem icon="blank" text="Compile on edit" />
+          </MenuItem>
         </Menu>
       </div>
     );
