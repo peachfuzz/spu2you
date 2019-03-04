66a89b5828f971e9d84160b03367cbfcd4982a7d
Alex Lopez <alexjlopez96@gmail.com>
2019-03-03 21:11:27 -0800

    Made splash image more responsive

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
-
 const splashImage = require('../../images/spu-splash-highres.jpg');
-
 // dimensions: 1600 × 1067
-
-// <img src={splashImage} className="w1600-h1067" alt="logo" />
-
 
 class Splash extends Component {
   render() {
