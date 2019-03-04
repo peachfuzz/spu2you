6a836ad394208d36c02ce8dd57c30f1810f4e1c9
Hector <hectordm65@gmail.com>
2019-03-03 18:42:16 -0800

    Fixed router crashing problems

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import {
   Alignment,
@@ -7,39 +7,68 @@ import {
   NavbarGroup,
   NavbarDivider
 } from "@blueprintjs/core";
+import { BrowserRouter as Router, withRouter } from "react-router-dom";
 
 class Footer extends Component {
   render() {
     return (
-      /* will want to eventually align these elements to the center of the footer div */ 
+      /* will want to eventually align these elements to the center of the footer div */
+
       <div className="App-footer">
-        <Navbar> 
-            <NavbarGroup align={Alignment.CENTER}>
-              <AnchorButton
-                  text="About"
-                  minimal
-                  icon="clean"
-                  onClick={ () => console.log("pressed button") /* this.props.history.push("/home") */ }
-                />
-                <NavbarDivider/>
-                <AnchorButton
-                    text="GitHub"
-                    minimal
-                    icon="git-branch"
-                    onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
-                />
-                <NavbarDivider/>
-                <AnchorButton
-                    text="Contact"
-                    minimal
-                    icon="envelope"
-                    onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
-                />
-            </NavbarGroup>
+        <Navbar>
+          <NavbarGroup align={Alignment.CENTER}>
+            <AnchorButton
+              text="About"
+              minimal
+              icon="clean"
+              onClick={
+                () => {
+                  this.props.history.push("/home");
+                  console.log("pressed button");
+                }
+                /* this.props.history.push("/home") */
+              }
+            />
+            <NavbarDivider />
+            <AnchorButton
+              text="GitHub"
+              minimal
+              icon="git-branch"
+              onClick={
+                () =>
+                  console.log(
+                    "pressed button"
+                  ) /* this.props.history.push("/") */
+              }
+            />
+            <NavbarDivider />
+            <AnchorButton
+              text="Contact"
+              minimal
+              icon="envelope"
+              onClick={
+                () =>
+                  console.log(
+                    "pressed button"
+                  ) /* this.props.history.push("/") */
+              }
+            />
+          </NavbarGroup>
+          "pressed button" ) /* this.props.history.push("/") */ } />
+          <NavbarDivider />
+          <AnchorButton
+            text="Contact"
+            minimal
+            icon="envelope"
+            onClick={
+              () =>
+                console.log("pressed button") /* this.props.history.push("/") */
+            }
+          />
         </Navbar>
       </div>
     );
   }
 }
 
-export default Footer;
+export default withRouter(Footer);
