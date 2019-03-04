7c2da955200cf994494da3279942ba8123fb9939
Hector <hectordm65@gmail.com>
2019-03-03 19:16:36 -0800

    fixed header stuff

--- src/components/static/Header.js
+++ src/components/static/Header.js
@@ -1,66 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Alignment,
-    AnchorButton,
-    Navbar,
-    NavbarGroup,
-    NavbarDivider,
-    NavbarHeading,
-  } from "@blueprintjs/core";
+  Alignment,
+  AnchorButton,
+  Navbar,
+  NavbarGroup,
+  NavbarDivider,
+  NavbarHeading
+} from "@blueprintjs/core";
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
 //docs: https://blueprintjs.com/docs/#blueprint
 
-const logoImage = require('../../images/virtual_student_logo_placeholder.png');
+const logoImage = require("../../images/virtual_student_logo_placeholder.png");
 
 class Header extends Component {
   render() {
-    return (
-      <div className="App-header">
-        <Navbar> 
-          {/* className={Classes.DARK} */}
+    if (this.props.location.pathname !== "/robot") {
+      return (
+        <div className="App-header">
+          <Navbar>
+            {/* className={Classes.DARK} */}
             <NavbarGroup align={Alignment.LEFT}>
-                <div className="header_logo">
-                  <img src={logoImage} className="w50-h50" alt="logo" />
-                </div>
-                <NavbarHeading>Virtual Student</NavbarHeading>
-                <NavbarDivider />
-                <AnchorButton
-                  text="Home"
-                  minimal
-                  icon="home"
-                  onClick={ () => this.props.history.push("/") }
-                />
-                <AnchorButton
-                    text="Create Reservation"
-                    minimal
-                    icon="calendar"
-                    onClick={ () => this.props.history.push("/calendar") }
-                />
-                <AnchorButton
-                    text="My Reservations"
-                    minimal
-                    icon="calendar"
-                    onClick={ () => this.props.history.push("/reservation") }
-                />
-                <AnchorButton
-                    text="Robot"
-                    minimal
-                    icon="rig"
-                    onClick={ () => this.props.history.push("/robot") }
-                />
+              <div className="header_logo">
+                <img src={logoImage} className="w50-h50" alt="logo" />
+              </div>
+              <NavbarHeading>Virtual Student</NavbarHeading>
+              <NavbarDivider />
+              <AnchorButton
+                text="Home"
+                minimal
+                icon="home"
+                onClick={() => this.props.history.push("/")}
+              />
+              <AnchorButton
+                text="Create Reservation"
+                minimal
+                icon="calendar"
+                onClick={() => this.props.history.push("/calendar")}
+              />
+              <AnchorButton
+                text="My Reservations"
+                minimal
+                icon="calendar"
+                onClick={() => this.props.history.push("/reservation")}
+              />
+              <AnchorButton
+                text="Robot"
+                minimal
+                icon="rig"
+                onClick={() => this.props.history.push("/robot")}
+              />
             </NavbarGroup>
             <NavbarGroup align={Alignment.RIGHT}>
-            <AnchorButton
+              <AnchorButton
                 text="Log Out"
                 minimal
                 rightIcon="log-in"
-                onClick={ () => this.props.history.push("/logout") }
-            />
+                onClick={() => this.props.history.push("/logout")}
+              />
             </NavbarGroup>
-        </Navbar>
-      </div>
-    );
+          </Navbar>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
   }
 }
 
-export default withRouter (Header);
+export default withRouter(Header);
