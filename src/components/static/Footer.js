9d82ea35e162fdc3416d937b4601390a296e23ba
Alex Lopez <alxjlopez96@gmail.com>
2019-03-03 18:44:52 -0800

    random changes idk

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -14,28 +14,28 @@ class Footer extends Component {
       /* will want to eventually align these elements to the center of the footer div */ 
       <div className="App-footer">
         <Navbar> 
-            <NavbarGroup align={Alignment.CENTER}>
+          <NavbarGroup align={Alignment.CENTER}>
+            <AnchorButton
+                text="About"
+                minimal
+                icon="clean"
+                onClick={ () => console.log("pressed button") /* this.props.history.push("/home") */ }
+              />
+              <NavbarDivider/>
               <AnchorButton
-                  text="About"
+                  text="GitHub"
                   minimal
-                  icon="clean"
-                  onClick={ () => console.log("pressed button") /* this.props.history.push("/home") */ }
-                />
-                <NavbarDivider/>
-                <AnchorButton
-                    text="GitHub"
-                    minimal
-                    icon="git-branch"
-                    onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
-                />
-                <NavbarDivider/>
-                <AnchorButton
-                    text="Contact"
-                    minimal
-                    icon="envelope"
-                    onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
-                />
-            </NavbarGroup>
+                  icon="git-branch"
+                  onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
+              />
+              <NavbarDivider/>
+              <AnchorButton
+                  text="Contact"
+                  minimal
+                  icon="envelope"
+                  onClick={ () => console.log("pressed button")   /* this.props.history.push("/") */ }
+              />
+          </NavbarGroup>
         </Navbar>
       </div>
     );
