e8999f87e07c19c1bcdcc59d87107360b4a2de02
Hector <hectordm65@gmail.com>
2019-03-04 14:34:28 -0800

    Updated the button so now logout works on the aws hosted version

---
+++ build/precache-manifest.a240d18deded5fe119b00d277e115fdd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1bd87971a332ecb21b4ac1d19537fa3",
+    "url": "/static/media/icons-20.f1bd8797.eot"
+  },
+  {
+    "revision": "7a0bfb68f17089f1f135",
+    "url": "/static/css/main.ad63f68d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5502b03b7b07c388f20f77fa469a1f47",
+    "url": "/static/media/icons-20.5502b03b.ttf"
+  },
+  {
+    "revision": "9662ee61a887bcdd7f8b",
+    "url": "/static/js/2.9662ee61.chunk.js"
+  },
+  {
+    "revision": "1d16a0bb0d5f60990e5dd53ec56b97e8",
+    "url": "/static/media/virtual_student_logo_placeholder.1d16a0bb.png"
+  },
+  {
+    "revision": "7a0bfb68f17089f1f135",
+    "url": "/static/js/main.7a0bfb68.chunk.js"
+  },
+  {
+    "revision": "a82b41082b04f774d82317dff6874d14",
+    "url": "/static/media/icons-16.a82b4108.ttf"
+  },
+  {
+    "revision": "7f037192fcdef96884010177e90e8c5f",
+    "url": "/static/media/icons-16.7f037192.eot"
+  },
+  {
+    "revision": "422aba8639312ac889d34200226a5c4e",
+    "url": "/static/media/icons-16.422aba86.woff"
+  },
+  {
+    "revision": "52f2f35bf3a635ca696e95cc33cf12fa",
+    "url": "/static/media/icons-20.52f2f35b.woff"
+  },
+  {
+    "revision": "9662ee61a887bcdd7f8b",
+    "url": "/static/css/2.3ec3e7d3.chunk.css"
+  },
+  {
+    "revision": "752ceea0ea6beac9016b2f417ad54b6f",
+    "url": "/index.html"
+  }
+];
