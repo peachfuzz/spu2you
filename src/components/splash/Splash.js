6dd92c10f1e18859db7e969e8a904f4e955a7ceb
Alex Lopez <alexjlopez96@gmail.com>
2019-03-03 21:49:28 -0800

    Splash screen login and signup card

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Card, Elevation } from "@blueprintjs/core";
 
 const splashImage = require('../../images/spu-splash-highres.jpg');
 // dimensions: 1600 × 1067
@@ -7,7 +8,24 @@ class Splash extends Component {
   render() {
     return (
       <div className="Splash">
-        <img src={splashImage} className="Splash-image" alt="logo" />
+        <img src={splashImage} className="Splash-image" alt="logo"/>
+        <div className="Splash-overlay">
+          <Card interactive={true} elevation={Elevation.TWO}>
+            <h1>Virtual Student</h1>
+            <div>
+              <Button onClick={() => this.props.history.push("/home")}>
+                Log In
+              </Button>
+            </div>
+            <br/>
+            <div>
+              <Button onClick={() => this.props.history.push("/home")}>
+                Sign Up
+              </Button>
+            </div>
+            <br/>
+          </Card>
+        </div>
       </div>
     );
   }
