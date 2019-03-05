45179619ec5d57b04435df499ca02544cbdfbd78
Hector <hectordm65@gmail.com>
2019-03-04 20:46:40 -0800

    Static content

---
+++ build/precache-manifest.2211e12f263c563cf07d2ac25d294cdc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f037192fcdef96884010177e90e8c5f",
+    "url": "/static/media/icons-16.7f037192.eot"
+  },
+  {
+    "revision": "24bb2ca81ccfeda70341",
+    "url": "/static/css/main.ad63f68d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5502b03b7b07c388f20f77fa469a1f47",
+    "url": "/static/media/icons-20.5502b03b.ttf"
+  },
+  {
+    "revision": "208d1c61aa39e08896f6",
+    "url": "/static/js/2.208d1c61.chunk.js"
+  },
+  {
+    "revision": "1d16a0bb0d5f60990e5dd53ec56b97e8",
+    "url": "/static/media/virtual_student_logo_placeholder.1d16a0bb.png"
+  },
+  {
+    "revision": "24bb2ca81ccfeda70341",
+    "url": "/static/js/main.24bb2ca8.chunk.js"
+  },
+  {
+    "revision": "f1bd87971a332ecb21b4ac1d19537fa3",
+    "url": "/static/media/icons-20.f1bd8797.eot"
+  },
+  {
+    "revision": "422aba8639312ac889d34200226a5c4e",
+    "url": "/static/media/icons-16.422aba86.woff"
+  },
+  {
+    "revision": "a82b41082b04f774d82317dff6874d14",
+    "url": "/static/media/icons-16.a82b4108.ttf"
+  },
+  {
+    "revision": "52f2f35bf3a635ca696e95cc33cf12fa",
+    "url": "/static/media/icons-20.52f2f35b.woff"
+  },
+  {
+    "revision": "208d1c61aa39e08896f6",
+    "url": "/static/css/2.08cd7a10.chunk.css"
+  },
+  {
+    "revision": "ec61e2352477ee8667bb0905ef8a3049",
+    "url": "/index.html"
+  }
+];
