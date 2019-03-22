346d0111e5b106b5e288857f55ab8a0356c75f51
Alex Lopez <alexjlopez96@gmail.com>
2019-03-21 22:14:47 -0700

    temp disable footer

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           <Header/>
           {/* <SideMenu/> */}
           <Content/>
-          <Footer/>
+          {/* <Footer/> */}
         </div>
       </Router>
     );
