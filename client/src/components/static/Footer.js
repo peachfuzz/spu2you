0e6369d93d710d7ab2b7b9b2684685fd75a6c4a1
Marissa <marm@spu.edu>
2019-04-18 14:23:10 -0700

    Some flexbox things
    some flexbox things for menu

--- client/src/components/static/Footer.js
+++ client/src/components/static/Footer.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <div className="App-footer" />;
+    return <div className="App-footer">
+    <small>
+    
+    </small>
+
+    <ul>
+    
+    </ul>
+    </div>
   }
 }
+
 
 export default Footer;
--- client/src/components/static/Header.js
+++ client/src/components/static/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
             <div className="header_logo">
               <img src={logoImage} className="w50-h50" alt="logo" />
             </div>
-            <NavbarHeading>Virtual Student</NavbarHeading>
+            <NavbarHeading><ul>Virtual Student</ul></NavbarHeading>
             <NavbarDivider />
             <AnchorButton
               text="Home"
