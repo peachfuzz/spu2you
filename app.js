7eba6c32edfff9962291a0a0f569c8d9a5993762
Hector <hectordm65@gmail.com>
2019-04-18 20:46:44 -0700

    put old backend code in a comment just in case

--- app.js
+++ app.js
@@ -226,6 +226,9 @@ app.get("/reservation", ensureAuthenticated, function(req, res) {
 app.get("/robot", ensureAuthenticated, function(req, res) {
   res.render("index", { user: req.user });
 });
+// app.get("*", ensureAuthenticated, (req, res) => { // this doesn't work :(
+//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
 
 // '/account' is only available to logged in user
 
@@ -292,3 +295,48 @@ app.get("/logout", function(req, res) {
 });
 
 app.listen(3000);
+
+// ! old app.js
+/*
+const express = require("express");
+const path = require("path");
+const request = require("request");
+const router = express.Router();
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use("/", router);
+
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   port: process.env.RDS_PORT
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("Database connection failed: " + err.stack);
+//     return;
+//   }
+
+//   console.log("Connected to database.");
+// });
+
+// connection.end();
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Backend listening on ${port}`);
+
+*/
