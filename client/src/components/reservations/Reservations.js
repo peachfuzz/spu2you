5213b1fbab9c3d5ca5ac3423d76c95c40b632316
Marissa <marm@spu.edu>
2019-05-01 14:29:41 -0700

    add button function to delete items and creating alert boxes to confirm cancellation of each

--- client/src/components/reservations/Reservations.js
+++ client/src/components/reservations/Reservations.js
@@ -1,34 +1,74 @@
 import React, { Component } from "react";
+//import Drawer from 'react-drag-drawer';
 //import { Code, getKeyComboString, KeyCombo, Card } from "@blueprintjs/core";
 import { Tag, Card, H5, Button } from "@blueprintjs/core";
 import Moment from "react-moment";
+import moment from "moment";
 import "moment-timezone";
 
-export default class Reservation extends Component {
-  constructor() {
-    super();
-    this.state = { selectedDate: new Date() };
+
+// create a drawer to show information when clicking a button
+class Reservations extends Component {
+  constructor(props) {
+    super(props);
+    var choice;
+  //  var msg;
+  //  var r = confirm("Are you sure you would like to cancel your reservation?");
+    //place holder for azure
+    this.state = {
+      list: [
+        { id: '1', name: "Hector", date: 2019, month: 3, day: 1},
+        { id: '2', name: "Alex", date: 2019, month: 2, day: 4 },
+        { id: '3', name: "Vanessa", date: 2019, month: 6, day: 8 },
+        { id: '4', name: "Marissa", date: 2019, month: 5, day: 10 },
+      ],
+    };
+
+    //if the reservation is cancelled then it will show it is 
   }
+
+  onRemoveItem = id => {
+    this.setState(state => {
+      const list = state.list.filter(item => item.id !== id);
+
+      return {
+        list,
+      };
+    });
+  };
+  
+  
+
+/*
+  // this is a pop up box 
+  alertBox = id =>{
+    if(r == true){
+      this.onRemoveItem(item.id);
+    //  msg  = "Reservation cancelled";
+    }
+    else{
+   //   msg = "Not cancelled";
+    }
+  }  // this.onRemoveItem(item.id)}
+  */
+
   render() {
     return (
-      <Card>
-        <H5>This is the date you chose:</H5>
-        <Tag key={this.state.selectedDate} icon="calendar">
-          <Moment date={this.state.selectedDate} format="LLLL" />
-        </Tag>
-        <p>To checkin to your reservation, click check-in</p>
-        <Button
-          rightIcon="arrow-right"
-          intent="success"
-          text="Check-in"
-          onClick={() => {
-            this.props.history.push({
-              pathname: "/robot",
-              state: { selectedDate: this.state.selectedDate }
-            });
-          }}
-        />
-      </Card>
+      <div>
+        <ul>
+          {this.state.list.map(item => (
+            <li key={item.id}>
+            
+              The reservation for {item.name} is {item.date} - {item.month} - {item.day} .
+              <button onClick={() => this.alertBox(item.id)}> 
+                Cancel Reservation
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
+
+  export default Reservations;
