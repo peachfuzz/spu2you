49af94333ada6ac6ff513aea553fee6a67356c71
Hector <hectordm65@gmail.com>
2019-05-04 14:24:28 -0700

    Made a smaller dates component

---
+++ client/src/components/calendar/Dates.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import { Tag, Button, Card, Colors, Divider } from "@blueprintjs/core";
+
+import Moment from "react-moment";
+import moment from "moment";
+import "moment-timezone";
+
+class Dates extends Component {
+  render() {
+    // return this.props.availableDates.map(time => {
+    //   return (
+    //     <p>
+    //       <Moment date={this.props.selectedDate + time} format="LLLL" />;
+    //       {this.props.selectedDate} {time}
+    //     </p>
+    //   );
+    // });
+    return <></>;
+  }
+}
+
+export default Dates;
