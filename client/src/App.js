184dddc842651fc2ef36bb6ccc247417058c9079
Hector <hectordm65@gmail.com>
2019-05-07 15:42:09 -0700

    Moved css references from js to app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
 import Content from "./components/router/Router.js";
 import Header from "./components/static/Header.js";
 import Footer from "./components/static/Footer.js";
 // import SideMenu from "./components/static/SideMenu.js";
-
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-//import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
-//import "@blueprintjs/select/lib/css/blueprint-select.css";
+import { FocusStyleManager } from "@blueprintjs/core";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 
+FocusStyleManager.onlyShowFocusOnTabs();
 class App extends Component {
     render() {
         return (
