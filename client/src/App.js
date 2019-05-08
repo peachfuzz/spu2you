0b379f25e77759af48dee6bd9620734565055bcd
Hector <hectordm65@gmail.com>
2019-05-08 05:56:37 -0700

    Removing pages we don't use

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Content from "./components/router/Router.js";
 import Header from "./components/static/Header.js";
-import Footer from "./components/static/Footer.js";
-// import SideMenu from "./components/static/SideMenu.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 class App extends Component {
@@ -11,9 +9,7 @@ class App extends Component {
             <Router>
                 <div className="App bp3-dark bp3-focus-disabled">
                     <Header />
-                    {/* <SideMenu /> */}
                     <Content />
-                    <Footer />
                 </div>
             </Router>
         );
