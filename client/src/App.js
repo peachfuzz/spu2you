844299b19d4f042091b9f8bd1fc11520839db63f
Hector <hectordm65@gmail.com>
2019-05-07 19:53:59 -0700

    Making the focus only show up on tab

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,13 @@ import Content from "./components/router/Router.js";
 import Header from "./components/static/Header.js";
 import Footer from "./components/static/Footer.js";
 // import SideMenu from "./components/static/SideMenu.js";
-import { FocusStyleManager } from "@blueprintjs/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-
-FocusStyleManager.onlyShowFocusOnTabs();
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="App bp3-dark">
+                <div className="App bp3-dark bp3-focus-disabled">
                     <Header />
                     {/* <SideMenu /> */}
                     <Content />
