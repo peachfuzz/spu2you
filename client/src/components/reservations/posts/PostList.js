74047bcd774c21325ac1ca7fdabd958b92aa0e8c
Hector <hectordm65@gmail.com>
2019-05-09 21:49:20 -0700

    Removed unecessary prop, fixed key indexing

--- client/src/components/reservations/posts/PostList.js
+++ client/src/components/reservations/posts/PostList.js
@@ -2,25 +2,25 @@ import React, { Component } from "react";
 
 import PostDetail from "./PostDetail";
 class PostList extends Component {
-	handleDateCallback(txtMsg) {
-		alert(txtMsg);
-		console.log(this);
-	}
-	render() {
-		const { post } = this.props;
-		return (
-			<div>
-				{this.props.PostData.map((item, index) => {
-					return (
-						<PostDetail
-							post={item}
-							key={"post-list-key ${index}"}
-							dateCallback={this.handleDateCallback}
-						/>
-					);
-				})}
-			</div>
-		);
-	}
+    handleDateCallback(txtMsg) {
+        alert(txtMsg);
+        console.log(this);
+    }
+    render() {
+        return (
+            <div>
+                {this.props.PostData.map((item, index) => {
+                    return (
+                        <PostDetail
+                            post={item}
+                            key={"post-list-key " + index}
+                            dateCallback={this.handleDateCallback}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
 }
+
 export default PostList;
