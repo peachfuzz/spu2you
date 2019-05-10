821722ecb93ab8d898445214dd7d805724400fa5
Hector <hectordm65@gmail.com>
2019-05-09 21:29:51 -0700

    Removed unecessary routes

--- client/src/components/router/Router.js
+++ client/src/components/router/Router.js
@@ -3,11 +3,8 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "../home/Home.js";
 import Calendar from "../calendar/Calendar.js";
-import Login from "../login/Login.js";
 import Reservations from "../reservations/Reservations.js";
 import Robot from "../robot/Robot";
-// import Splash from '../splash/Splash.js';
-// <Route path="/splash" component={Splash} />
 
 class Content extends Component {
     render() {
@@ -16,7 +13,6 @@ class Content extends Component {
                 <div className="inner-content">
                     <Switch>
                         <Route exact path="/" component={Home} />
-                        <Route path="/login" component={Login} />
                         <Route path="/calendar" component={Calendar} />
                         <Route path="/robot" component={Robot} />
                         <Route path="/reservations" component={Reservations} />
